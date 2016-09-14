8c376690668f11cf16e3a403c8996d2bd52e624a
Mauro Daprotis <maurodaprotis@gmail.com>
2016-09-14 18:42:50 -0300

    implementado move en tanque jugador

--- proyectoBC/entities/tanques/jugadores/TanqueJugador.java
+++ proyectoBC/entities/tanques/jugadores/TanqueJugador.java
@@ -1,7 +1,7 @@
 package proyectoBC.entities.tanques.jugadores;
 
 import javax.swing.ImageIcon;
-
+import java.awt.event.KeyEvent;
 import proyectoBC.entities.tanques.Tanque;
 
 public class TanqueJugador extends Tanque {
@@ -21,8 +21,25 @@ public class TanqueJugador extends Tanque {
 		System.out.println("Tanque jugador Creado");
 	}
 	public void move(int dir,int dist){
-		/* Algo */
-		super.move(dir);
+		int distance = (dist > this.speed) ? this.speed:dist;
+		switch (dir){
+		case KeyEvent.VK_UP : 
+			this.position.setLocation(this.position.x, this.position.y - distance);
+			super.move(MOVE_UP);
+			break;
+		case KeyEvent.VK_DOWN :
+			this.position.setLocation(this.position.x, this.position.y + distance);
+			super.move(MOVE_DOWN);
+			break;
+		case KeyEvent.VK_LEFT :
+			this.position.setLocation(this.position.x - distance, this.position.y);
+			super.move(MOVE_LEFT);
+			break;
+		case KeyEvent.VK_RIGHT :
+			this.position.setLocation(this.position.x + distance, this.position.y);
+			super.move(MOVE_RIGHT);
+			break;
+		}		
 	}
 	public void impact(){		  
 	}
